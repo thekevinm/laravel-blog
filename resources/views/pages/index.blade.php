7920b1e2624372b70c7710aec1dee0fd24181fe2
Kevin McGahey <kevinmcgahey1114@gmail.com>
2018-07-23 11:12:17 -0700

    minor style adjustments

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -4,6 +4,8 @@
     <div class="jumbotron text-center" style="margin-top:180px;">
         <h1>{{$title}}</h1>
         <br>
+        <h4>Login or Register to create posts!</h4>
+        <br>
         <p>
             <a class="btn btn-primary btn-lg" href="/login" role="button">Login</a>
             <a class="btn btn-success btn-lg" href="/register" role="button">Register</a>
